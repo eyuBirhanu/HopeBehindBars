9a2db8e99af783709b81478aff936913a08f0664
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-07-22 08:55:30 +0300

    fix: configure vite base path for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' 
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+
   plugins: [react()],
   server: {
     proxy: {
-      // Any request starting with /api will be proxied
       '/api': {
-        target: 'http://localhost:5001', // Your Express server's address
-        changeOrigin: true, // This is important for CORS
+        target: 'http://localhost:5001',
+        changeOrigin: true,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
